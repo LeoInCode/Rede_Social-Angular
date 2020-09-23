464393eb54f0290f23636a3b60733f7c02e3500d
LeoInCode <lsm.ccomp@gmail.com>
2020-09-23 15:51:02 -0300

    Instalando boostrap 4 e ngx-bootstrap

---
+++ frontend/redeSocial/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { LoginComponent } from './firstPage/login/login.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
