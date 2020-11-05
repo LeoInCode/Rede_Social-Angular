f81ff2e30bb3a9ff5d1ee8520bba933d36209825
LeoInCode <lsm.ccomp@gmail.com>
2020-11-05 13:52:35 -0300

    Adicionando método get ao service de jogos

--- frontend/redeSocial/src/app/shared/services/jogos/jogos.service.ts
+++ frontend/redeSocial/src/app/shared/services/jogos/jogos.service.ts
@@ -1,3 +1,5 @@
+import { take } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,12 @@ import { Injectable } from '@angular/core';
 })
 export class JogosService {
 
-  constructor() { }
+  URL: string = 'http://localhost:3000/jogos';
+
+  constructor(private http: HttpClient) { }
+
+  getJogos() {
+    return this.http.get(this.URL).pipe(take(1));
+  }
+
 }
