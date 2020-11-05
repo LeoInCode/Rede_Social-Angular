06ba72e8afed7b3c98c6b3ab6c78da32688db204
LeoInCode <lsm.ccomp@gmail.com>
2020-11-05 14:00:28 -0300

    Utilizando implementação do service jogo no HomeComponent

--- frontend/redeSocial/src/app/home/home.component.ts
+++ frontend/redeSocial/src/app/home/home.component.ts
@@ -1,3 +1,5 @@
+import { JogosService } from './../shared/services/jogos/jogos.service';
+import { Jogo } from './../model/Jogo';
 import { FeedService } from './../shared/services/feed/feed.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Feed } from './../model/Feed';
@@ -19,12 +21,14 @@ export class HomeComponent implements OnInit {
   usuario: Usuario;
   feed: Feed[];
   formulario: FormGroup;
+  jogos: Jogo[];
 
   constructor(private route: ActivatedRoute,
               private userServicer: UsuariosService,
               private userContext: UserContextService,
               private formBuilder: FormBuilder,
-              private feedService: FeedService) { }
+              private feedService: FeedService,
+              private jogoService: JogosService) { }
 
   ngOnInit(): void {
     this.getUser();
@@ -48,6 +52,12 @@ export class HomeComponent implements OnInit {
         this.userContext.user = data;
       })
     }
+  }
+
+  getJogos(){
+    this.jogoService.getJogos().subscribe((data: Jogo[]) => {
+      this.jogos = [...data];
+    })
   }
   
   public(){
