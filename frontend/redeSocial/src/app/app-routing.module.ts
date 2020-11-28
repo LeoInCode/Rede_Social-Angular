64428666f7a903932158da376cf849cb45a9d3bd
LeoInCode <lsm.ccomp@gmail.com>
2020-11-28 11:18:13 -0300

    Adicionando first-page-rounting.module.ts e fazendo lazzy loading

--- frontend/redeSocial/src/app/app-routing.module.ts
+++ frontend/redeSocial/src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 import { UsuarioResolver } from './shared/services/usuarios/usuario.resolver';
 import { JogoResolver } from './shared/services/jogos/jogo.resolver';
 import { JogoComponent } from './jogo/jogo.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { HomeComponent } from './home/home.component';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
-import { CadastroComponent } from './first-page/cadastro/cadastro.component';
-import { FirstPageComponent } from './first-page/first-page.component';
-import { LoginComponent } from './first-page/login/login.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', component: FirstPageComponent
+    loadChildren: () => import('./first-page/first-page.module').then((m) => m.FirstPageModule),
   },
   {
     path: 'home/:nick', component: HomeComponent,   
